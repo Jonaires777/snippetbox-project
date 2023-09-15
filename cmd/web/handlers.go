4b8ca8a1095273bfdff01d5fe3e8be2ae7814abe
Jonathan Aires <airesjonathan123@gmail.com>
2023-09-15 17:37:30 -0300

    Multiple-records SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"text/template"
 
 	"snippetbox.project/internal/models"
 )
@@ -16,21 +15,29 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
-		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v", snippet)
 	}
+	//	files := []string{
+	//		"./ui/html/base.tmpl.html",
+	//		"./ui/html/partials/nav.tmpl.html",
+	//		"./ui/html/pages/home.tmpl.html",
+	//	}
+	//	ts, err := template.ParseFiles(files...)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//		return
+	//	}
+	//
+	//	err = ts.ExecuteTemplate(w, "base", nil)
+	//	if err != nil {
+	//		app.serverError(w, err)
+	//	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
